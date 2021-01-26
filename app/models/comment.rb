f1bd8a4ecc59a5df8e83c4d89ee849128780f970
Michael Fuget <michaelfuget@gmail.com>
2021-01-26 06:36:15 +0000

    Automatically backed up by Learn

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,5 +2,10 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
   accepts_nested_attributes_for :user
+  def user_attributes=(user_attributes)
+    self.user = User.find_or_create_by(user_attributes)
+    category = Category.find_or_create_by(category_attribute)
+    self.user  << category
+  end
 
 end
