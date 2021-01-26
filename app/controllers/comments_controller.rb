69a926fc57d7ebd984ddd4f29a06cc3e07be03b4
Michael Fuget <michaelfuget@gmail.com>
2021-01-26 07:27:59 +0000

    Automatically backed up by Learn

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,7 +4,7 @@ class CommentsController < ApplicationController
     puts params
     comment = Comment.create(comment_params)
     comment.user = User.find(params[:comment][:user_id])
-    puts comment.user.first.username
+    puts comment.user.username
     redirect_to comment.post
   end
 
