e67d3e8419d3a2f23a1533e5531e5c4a6fdfc1d5
Michael Fuget <michaelfuget@gmail.com>
2021-01-26 06:27:15 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -22,6 +22,6 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:title, :content, category_ids:[], categories_attributes: [:name], comment_ids:[], comment_ids: [:username])
+    params.require(:post).permit(:title, :content, category_ids:[], categories_attributes: [:name], comment_ids:[], comments_attributes: [:username])
   end
 end
