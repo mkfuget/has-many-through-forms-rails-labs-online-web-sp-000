67e05a3e0fc54a3b9dd91656495b3c921dd2149a
Michael Fuget <michaelfuget@gmail.com>
2021-01-26 07:11:22 +0000

    Automatically backed up by Learn

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,8 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
   accepts_nested_attributes_for :user
+  accepts_nested_attributes_for :post
+
   def post_id=(post_id)
     if !post_id.blank?
       self.post = Post.find(post_id)
